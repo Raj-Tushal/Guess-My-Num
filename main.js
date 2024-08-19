9b2423b64563bc851cc2912bad3090039a8ca3de
Raj Tushal <tushalkhatri902@gmail.com>
2024-08-19 23:36:23 +0500

    guess my num updated

--- main.js
+++ main.js
@@ -1,82 +1,73 @@
 const checkBtn = document.querySelector(".checkBtn");
 const userNo = document.querySelector(".userNo");
-const machineNo = document.querySelector(".mahineNo");
-const againBtn = document.querySelector(".again")
-const score = document.querySelector(".score")
-const highScore = document.querySelector(".highScore")
-const message = document.querySelector(".message")
-
-// users value
-// const userValue =userNo.value;
-// machine Value
-
-// function generateRandomNumber() {
-//     return Math.floor(Math.random() * 20) + 1;
-// }
-
-
-
-// checkBtn.addEventListener("click",generateRandomNumber)
-
-// if (userValue==machineValue) {
-//     machineNo.innerHTML=userValue;
-// }
-// else{
-//     machineNo.innerHTML="try!";
-// }
-
-
-// if (userNo.value==machineNo.innerHTML) {
-//     machineNo.innerHTML="matched"
-// }
+const machineNo = document.querySelector(".machineNo");
+const againBtn = document.querySelector(".again");
+const score = document.querySelector(".score");
+const highScore = document.querySelector(".highScore");
+const message = document.querySelector(".message");
+const mahineNo = document.querySelector(".mahineNo");
 
 let currentScore = 20;
 let maxScore = 0;
+let randomNum = generateRandomNumber(); 
 
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 20) + 1;
+}
 
-checkBtn.addEventListener("click",()=>{
-    function generateRandomNumber() {
-        return Math.floor(Math.random() * 20) + 1;
-    }
-    score.innerHTML=20;
-    const userNum = parseInt(userNo.value, 10); 
-    const randomNum = generateRandomNumber();
-    const Score = parseInt(score.innerHTML, 10); 
+checkBtn.addEventListener("click", () => {
+    const userNum = parseInt(userNo.value, 10);
 
     console.log(randomNum);
     console.log(userNum);
 
-    if (randomNum===userNum) {
-        machineNo.innerHTML=userNum;
-        message.style.display="block";
-       
-        if (currentScore>maxScore) {
-            maxScore =currentScore;
-            highScore.textContent=currentScore;         
-            score.textContent = 0;
-            currentScore=20;
+    if (randomNum === userNum) {
+        mahineNo.innerHTML = userNum;
+        message.style.display = "block";
+        message.innerHTML = "Congratulations! </br> You've guessed it right.";
+        
+        mahineNo.style.backgroundColor = "red";
+
+        setTimeout(()=>{ 
+            userNo.value = '';
+            mahineNo.innerHTML = '?';
+            message.style.display = "none";
+            currentScore = 20;
+            score.textContent = currentScore;
+            location.reload(location.reload())},5000);
+
+        if (currentScore > maxScore) {
+            maxScore = currentScore;
+            highScore.textContent = currentScore;
         }
-    }
-    else{
-        machineNo.innerHTML="?";
+
+        score.textContent = 0;
+        currentScore = 20;
+    } else if (userNum < randomNum) {
+        message.style.display = "block";
+        message.innerHTML = "My number is greater than " + userNum;
+    } else if (userNum > randomNum) {
+        message.style.display = "block";
+        message.innerHTML = "My number is less than " + userNum;
+    } else {
+        machineNo.innerHTML = "?";
         currentScore--;
         score.textContent = currentScore;
 
-        if (currentScore<=0) {
-            highScore.textContent=0;
-            alert("Game Over!, Try Again")
+        if (currentScore <= 0) {
+            highScore.textContent = 0;
+            alert("Game Over! Try Again");
             location.reload();
         }
     }
-})
+});
 
-
-
-
-
-// again btn 
-againBtn.addEventListener("click",()=>{
-    location.reload();
-    userNo.value=0;
-})
-
+// again button
+againBtn.addEventListener("click", () => {
+    randomNum = generateRandomNumber(); 
+    userNo.value = '';
+    machineNo.innerHTML = '?';
+    message.style.display = "none";
+    currentScore = 20;
+    score.textContent = currentScore;
+});
